ff438c77588c58bf9d7d0666113d736104fc2c1b
login256 <ahaloer@163.com>
2019-01-30 17:41:50 +0800

    对第三章主要部分更改

--- 第三章/debug_code/main-changed.c
+++ 第三章/debug_code/main-changed.c
@@ -1,7 +1,7 @@
 /*
  * Debug示例程序 
  * 程序目的是读入两个数A B 输出A*B-(A+B)的值
- * 该程序有大量错误
+ * 
  */
 
 #include <stdio.h>
@@ -9,10 +9,10 @@
 int main()
 {
 	int a,b,c,d,e;
-	scanf("%d%d",&a,b);
-	c=a*a;
+	scanf("%d%d",&a,&b);
+	c=a*b;
 	d=a+b;
 	e=c-d;
-	printf("%d",d);
+	printf("%d",e);
 	return 0;
 }
